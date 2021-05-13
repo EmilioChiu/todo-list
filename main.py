66187f98fb723d9d9fa7fd63723faa09040716bb
Emilio Chiu <espinosachiuemilio@outlook.es>
2021-05-13 18:33:01 -0500

    change to sqlite to postgres

--- main.py
+++ main.py
@@ -4,13 +4,14 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
+import os
 
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "asd241ihiep193zzz"
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todos.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
